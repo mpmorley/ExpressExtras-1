e2fcc953dd68efc8a1ab51fdd07239325ae9c3f7
bapoorva <apoorva.babu4@gmail.com>
2020-02-27 10:51:30 -0500

    Fix dplyr error

--- R/Cleanup2.R
+++ R/Cleanup2.R
@@ -9,8 +9,8 @@
 Cleanup2 <-function(tt){
   data('ptntype')
   res <- tt %>% mutate(fc = ifelse(logFC<0, -1*2^abs(logFC),2^logFC)) %>%
-    select(ENSEMBL,SYMBOL,ENTREZID,biotype,geneloc,logFC,fc,P.Value,adj.P.Val,t)
-  res=left_join(res,ptntype,by=c("ENSEMBL"="ENSEMBL")) %>% select(-gene) %>% select(ENSEMBL:geneloc,protein_type,logFC:t)
+    dplyr::select(ENSEMBL,SYMBOL,ENTREZID,biotype,geneloc,logFC,fc,P.Value,adj.P.Val,t)
+  res=left_join(res,ptntype,by=c("ENSEMBL"="ENSEMBL")) %>% dplyr::select(-gene) %>% dplyr::select(ENSEMBL:geneloc,protein_type,logFC:t)
   rownames(res) <- rownames(tt)
   return(res)
 }
